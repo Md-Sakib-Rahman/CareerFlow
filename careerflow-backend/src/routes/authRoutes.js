ba34c66c21e0c6f9d29a884370a4c7e0738c2bb9
saifur-sagor <mdsaifurrahmansagor606@gmail.com>
2026-02-23 11:16:20 +0600

    [ADDED]-forget-password,reset-password and update profile end-point

--- careerflow-backend/src/routes/authRoutes.js
+++ careerflow-backend/src/routes/authRoutes.js
@@ -1,12 +1,23 @@
-const express = require("express")
-const { registerUser, loginUser , refreshTokenController} = require("../controllers/authController")
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  refreshTokenController,
+  forgotPassword,
+  resetPassword,
+  getMe,
+  updateMe,
+} = require("../controllers/authController");
 
-const router = express.Router()
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
 
-
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.post('/refreshtoken', refreshTokenController)
-
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refreshtoken", refreshTokenController);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+router.get("/me", protect, getMe); // 'protect' middleware to ensure user is authenticated before accessing this route
+router.post("/updateMe", protect, updateMe);
 
 module.exports = router;
